3f467b8da0bbe1c1e3a30493d7c51234f60a2bb3
eric-bach <bach.eric@gmail.com>
2025-07-30 16:15:32 -0600

    Update to use Link tag

--- frontend/app/(home)/layout.tsx
+++ frontend/app/(home)/layout.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React from 'react';
+import Link from 'next/link';
 import {
   Authenticator,
   Button,
@@ -36,12 +37,12 @@ function ChatLayout({ children, signOut, user }: ChatLayoutProps) {
       {/* Navbar */}
       <nav className='flex-shrink-0 bg-card border-b border-border px-4 py-3'>
         <div className='max-w-4xl mx-auto flex items-center justify-between'>
-          <a href='/' className='href'>
+          <Link href='/' className='href'>
             <div className='flex items-center space-x-3'>
               <img src='/logo.png' alt='RAGBot Logo' className='w-8 h-8' />
               <h1 className='text-xl font-semibold'>RAGBot 2</h1>
             </div>
-          </a>
+          </Link>
           <UserProfileDropdown user={user} signOut={signOut} />
         </div>
       </nav>
